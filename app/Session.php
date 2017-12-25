8ab6a6f53821c544d7338b2f67cb88765367dd15
Ubuntu <thanhtung.uet@gmail.com>
2017-12-25 12:12:08 +0700

    Update

--- app/Session.php
+++ app/Session.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Session extends Model
 {
+    /**
+     * Return the employee who created this session
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
 
+    public function employee() {
+        return $this->belongsTo('App\Employee', 'employee_id');
+    }
 }
--- app/Ticket.php
+++ app/Ticket.php
@@ -6,11 +6,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+    /**
+     * Return the employee who this ticket is created_by
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+
     public function created_by_employee() {
         return $this->belongsTo('App\Employee', 'created_by');
     }
 
+    /**
+     * Return the employee who this ticket is assigned to
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+
     public function assigned_to_employee() {
         return $this->belongsTo('App\Employee', 'assigned_to');
+    }
+
+    /**
+     * Return relaters of this ticket
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+
+    public function relaters() {
+        return $this->hasMany('App\TicketRelater', 'ticket_id');
+    }
+
+    /**
+     * Return comments in this ticket
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+
+    public function comments() {
+        return $this->hasMany('App\TicketThread', 'ticket_id');
     }
 }
